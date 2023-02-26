66feb798dbfff9b35830edeb1cf6006d40437c22
Арман <armanukk777@gmail.com>
2023-02-26 16:11:22 +0600

    Задачи 34 и 38

---
+++ exercise34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 999);
+}
+
+int CountNum(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 ==0)
+      count++;
+  return count;
+}
+
+void PrintArray(int[] array)
+{
+  foreach (var el in array)
+    Console.Write($"{el} ");
+  Console.WriteLine();
+}
+
+int[] array = new int[5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"-> {CountNum(array)}");
---
+++ exercise38/Program.cs
@@ -0,0 +1,28 @@
+﻿// 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3 7 22 2 78] -> 76
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+
+string DiffArray(int [] array)
+{
+    string result = "";
+    int max = array[0];
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > max)
+            max = array[i];
+    for (int i = 0; i < array.Length; i++)
+        if (min > array[i])
+            min = array[i];
+    result = $"[{string.Join(", ", array)}] -> {max - min}";
+    return result;
+}
+
+
+int[] array = new int[5];
+FillArray(array);
+Console.WriteLine(DiffArray(array));
