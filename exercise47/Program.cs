b62d0d80a7fe8c157a87faf2e103fedf8bfe4fd5
Арман <armanukk777@gmail.com>
2023-03-04 21:16:29 +0600

    47 и 50

---
+++ exercise47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        array[i, j] = Convert.ToDouble(rnd.Next(-10, 10));
+    }
+}
+
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
